75882b430a12d0af9998cd72b44a8e9d565835ed
Amir Sohail <amir.sohail@brainvire.com>
2024-04-25 16:15:01 +0530

    modified config

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const BASE_PROFILE_IMAGE_URL='http://localhost:4000/public/uploads/profiles/';
-export const BASE_POST_IMAGE_URL='http://localhost:4000/public/uploads/posts/';
+export const BASE_PROFILE_IMAGE_URL='http://192.168.10.103:4000/public/uploads/profiles/';
+export const BASE_POST_IMAGE_URL='http://192.168.10.103:4000/public/uploads/posts/';
 
 export const SOCKET_ENDPOINT = "http://192.168.10.103:4000";
 
